999153340bf937fd60e871a3a386d6ec9b973dbc
xzonzh@gmail.com <xzonzh@gmail.com@d5b1f9fa-850a-afc6-c9b0-4d056de4802f>
2011-08-30 10:13:30 +0000

    PathTransitionDemo

---
+++ trunk/hxzon.demo/org/hxzon/demo/javafx/PathTransitionDemo.java
@@ -0,0 +1,56 @@
+package org.hxzon.demo.javafx;
+
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class PathTransitionDemo extends Application {
+
+	@Override
+	public void start(Stage stage) {
+		Group p = new Group();
+		Scene scene = new Scene(p);
+		stage.setScene(scene);
+		stage.setWidth(500);
+		stage.setHeight(500);
+		p.setTranslateX(80);
+		p.setTranslateY(80);
+
+		//rect
+		final Rectangle rect = new Rectangle(0, 0, 40, 40);
+		rect.setArcHeight(10);
+		rect.setArcWidth(10);
+		rect.setFill(Color.ORANGE);
+		//path
+		Path path = new Path();
+		path.getElements().add(new MoveTo(20, 20));
+		path.getElements().add(new CubicCurveTo(380, 0, 380, 120, 200, 120));
+		path.getElements().add(new CubicCurveTo(0, 120, 0, 240, 380, 240));
+		//path transition
+		PathTransition pathTransition = new PathTransition();
+		pathTransition.setDuration(Duration.valueOf(4000));
+		pathTransition.setPath(path);
+		pathTransition.setNode(rect);
+		pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+		pathTransition.setCycleCount(Timeline.INDEFINITE);
+		pathTransition.setAutoReverse(true);
+		pathTransition.play();
+		
+		p.getChildren().add(rect);
+		
+		stage.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
