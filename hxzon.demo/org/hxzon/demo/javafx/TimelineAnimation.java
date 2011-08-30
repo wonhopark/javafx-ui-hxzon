b98d7b78ef1d0275e755c3ffcd349a4faf9633d2
xzonzh <xzonzh@gmail.com>
2011-08-30 10:23:10 +0000

    TimelineAnimation

---
+++ hxzon.demo/org/hxzon/demo/javafx/TimelineAnimation.java
@@ -0,0 +1,46 @@
+package org.hxzon.demo.javafx;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class TimelineAnimation extends Application {
+
+	@Override
+	public void start(Stage stage) {
+		Group p = new Group();
+		Scene scene = new Scene(p);
+		stage.setScene(scene);
+		stage.setWidth(500);
+		stage.setHeight(500);
+		p.setTranslateX(80);
+		p.setTranslateY(80);
+
+		final Rectangle rect = new Rectangle(100, 50, 100, 50);
+		rect.setFill(Color.BROWN);
+
+		final Timeline timeline = new Timeline();
+		timeline.setCycleCount(Timeline.INDEFINITE);
+		timeline.setAutoReverse(true);
+		final KeyValue kv = new KeyValue(rect.xProperty(), 300);
+		final KeyFrame kf = new KeyFrame(Duration.valueOf(500), kv);
+		timeline.getKeyFrames().add(kf);
+		timeline.play();
+
+		p.getChildren().add(rect);
+
+		stage.setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
