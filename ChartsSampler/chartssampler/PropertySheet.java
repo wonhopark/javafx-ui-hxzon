38658f34667204873635922c2d5fd12296fb104b
xzonzh <xzonzh@gmail.com>
2011-10-10 09:24:17 +0000

    (update for javafx-2.0) ReadOnlyDoubleProperty ValueAxis.scaleProperty()

--- ChartsSampler/chartssampler/PropertySheet.java
+++ ChartsSampler/chartssampler/PropertySheet.java
@@ -34,10 +34,15 @@ package chartssampler;
 import java.util.Arrays;
 import java.util.List;
 
+import javafx.beans.binding.BooleanExpression;
+import javafx.beans.binding.NumberExpressionBase;
+import javafx.beans.binding.ObjectExpression;
+import javafx.beans.binding.StringExpression;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -51,7 +56,7 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 
@@ -62,28 +67,34 @@ public class PropertySheet extends GridPane {
 
     private final List<PropertyGroup> propertyGroups;
 
-    public PropertySheet(PropertyGroup ... propertyGroups) {
+    public PropertySheet(PropertyGroup... propertyGroups) {
         getStyleClass().add("property-sheet");
         this.propertyGroups = Arrays.asList(propertyGroups);
         int row = 0;
-        for(PropertyGroup pg: propertyGroups) row = pg.create(this,row);
+        for (PropertyGroup pg : propertyGroups)
+            row = pg.create(this, row);
     }
 
     public static Property createProperty(String name, EventHandler<ActionEvent> action) {
         return new ActionPropertyImpl(name, action);
     }
+
     public static Property createProperty(String name, StringProperty prop) {
         return new StringPropertyImpl(name, prop);
     }
+
     public static Property createProperty(String name, IntegerProperty prop) {
         return new IntegerPropertyImpl(name, prop);
     }
+
     public static Property createProperty(String name, DoubleProperty prop) {
         return new DoublePropertyImpl(name, prop);
     }
+
     public static Property createProperty(String name, BooleanProperty prop) {
         return new BooleanPropertyImpl(name, prop);
     }
+
     public static Property createProperty(String name, ObjectProperty prop) {
         if (prop.getValue() instanceof Enum) {
             return new EnumPropertyImpl(name, prop);
@@ -92,11 +103,16 @@ public class PropertySheet extends GridPane {
         }
     }
 
+    //add by hxzon
+    public static Property createProperty(String name, NumberExpressionBase prop) {
+        return new ReadOnlyPropertyImpl(name, prop);
+    }
+
     public static class PropertyGroup {
         private final String title;
         private final Property[] properties;
 
-        public PropertyGroup(String title,Property ... properties) {
+        public PropertyGroup(String title, Property... properties) {
             this.title = title;
             this.properties = properties;
         }
@@ -105,43 +121,43 @@ public class PropertySheet extends GridPane {
             Label titleLabel = new Label(title);
             titleLabel.getStyleClass().add("property-sheet-header");
             titleLabel.setMaxWidth(Double.MAX_VALUE);
-            GridPane.setMargin(titleLabel, new Insets((row==0)?0:5,0,5,0));
+            GridPane.setMargin(titleLabel, new Insets((row == 0) ? 0 : 5, 0, 5, 0));
             GridPane.setColumnIndex(titleLabel, 0);
             GridPane.setColumnSpan(titleLabel, 2);
             GridPane.setHgrow(titleLabel, Priority.ALWAYS);
             GridPane.setRowIndex(titleLabel, row);
             grid.getChildren().add(titleLabel);
-            row ++;
-            for(Property prop: properties) {
-                if (prop == null){
+            row++;
+            for (Property prop : properties) {
+                if (prop == null) {
                     continue;
                 } else if (prop instanceof ActionPropertyImpl) {
-                    Label propLabel = new Label(prop.getName()+":");
+                    Label propLabel = new Label(prop.getName() + ":");
                     Node editor = prop.getEditor();
                     GridPane.setHalignment(editor, HPos.CENTER);
                     GridPane.setHgrow(editor, Priority.ALWAYS);
-                    GridPane.setMargin(editor, new Insets(2,2,2,2));
+                    GridPane.setMargin(editor, new Insets(2, 2, 2, 2));
                     GridPane.setColumnIndex(editor, 0);
                     GridPane.setColumnSpan(editor, 2);
                     GridPane.setRowIndex(editor, row);
                     grid.getChildren().add(editor);
-                    row ++;
+                    row++;
                 } else {
-                    Label propLabel = new Label(prop.getName()+":");
+                    Label propLabel = new Label(prop.getName() + ":");
                     propLabel.setMaxWidth(Double.MAX_VALUE);
                     propLabel.setAlignment(Pos.CENTER_RIGHT);
                     GridPane.setHalignment(propLabel, HPos.RIGHT);
-                    GridPane.setMargin(propLabel, new Insets(2,2,2,2));
-                    GridPane.setColumnIndex(propLabel,0);
+                    GridPane.setMargin(propLabel, new Insets(2, 2, 2, 2));
+                    GridPane.setColumnIndex(propLabel, 0);
                     GridPane.setRowIndex(propLabel, row);
                     grid.getChildren().add(propLabel);
                     Node editor = prop.getEditor();
                     GridPane.setHgrow(editor, Priority.ALWAYS);
-                    GridPane.setMargin(editor, new Insets(2,2,2,2));
-                    GridPane.setColumnIndex(editor,1);
+                    GridPane.setMargin(editor, new Insets(2, 2, 2, 2));
+                    GridPane.setColumnIndex(editor, 1);
                     GridPane.setRowIndex(editor, row);
                     grid.getChildren().add(editor);
-                    row ++;
+                    row++;
                 }
             }
             return row;
@@ -162,6 +178,36 @@ public class PropertySheet extends GridPane {
         public abstract Node getEditor();
     }
 
+    //add by hxzon
+    static class ReadOnlyPropertyImpl extends Property {
+        private Label editor = new Label();
+
+        ReadOnlyPropertyImpl(String name, NumberExpressionBase prop) {
+            super(name);
+            editor.setText(prop.getValue().toString());
+        }
+
+        ReadOnlyPropertyImpl(String name, BooleanExpression prop) {
+            super(name);
+            editor.setText(prop.getValue().toString());
+        }
+
+        ReadOnlyPropertyImpl(String name, StringExpression prop) {
+            super(name);
+            editor.setText(prop.getValue());
+        }
+
+        ReadOnlyPropertyImpl(String name, ObjectExpression prop) {
+            super(name);
+            editor.setText(prop.getValue().toString());
+        }
+
+        @Override
+        public Node getEditor() {
+            return editor;
+        }
+    }
+
     static class ActionPropertyImpl extends Property {
         private Hyperlink editor = new Hyperlink();
 
@@ -171,82 +217,95 @@ public class PropertySheet extends GridPane {
             editor.setOnAction(action);
         }
 
-        @Override public Node getEditor() {
+        @Override
+        public Node getEditor() {
             return editor;
         }
     }
+
     static class StringPropertyImpl extends Property {
-        private TextBox editor = new TextBox();
+        private TextField editor = new TextField();
         private String value = null;
 
         StringPropertyImpl(String name, final StringProperty sp) {
             super(name);
             editor.setText(sp.getValue());
             sp.addListener(new ChangeListener<String>() {
-                @Override public void changed(ObservableValue<? extends String> valueModel, String oldValue, String newValue) {
+                @Override
+                public void changed(ObservableValue<? extends String> valueModel, String oldValue, String newValue) {
                     if (value == null || !value.equals(newValue))
-                            editor.setText(valueModel.getValue());
+                        editor.setText(valueModel.getValue());
                 }
             });
-            editor.rawTextProperty().addListener(new ChangeListener<String>() {
-                @Override public void changed(ObservableValue<? extends String> valueModel, String oldValue, String newValue) {
+            editor.textProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> valueModel, String oldValue, String newValue) {
                     if (value == null || !value.equals(newValue))
-                         sp.setValue(valueModel.getValue());
+                        sp.setValue(valueModel.getValue());
                 }
             });
         }
 
-        @Override public Node getEditor() {
+        @Override
+        public Node getEditor() {
             return editor;
         }
     }
 
     static class DoublePropertyImpl extends Property {
-        private TextBox editor = new TextBox();
+        private TextField editor = new TextField();
 
         DoublePropertyImpl(String name, final DoubleProperty dp) {
             super(name);
             editor.setText(Double.toString(dp.getValue()));
             dp.addListener(new ChangeListener<Number>() {
-                @Override public void changed(ObservableValue<? extends Number> valueModel, Number oldValue, Number newValue) {
+                @Override
+                public void changed(ObservableValue<? extends Number> valueModel, Number oldValue, Number newValue) {
                     editor.setText(newValue.toString());
                 }
             });
-            editor.rawTextProperty().addListener(new ChangeListener<String>() {
-                @Override public void changed(ObservableValue<? extends String> valueModel, String oldValue, String newValue) {
+            editor.textProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> valueModel, String oldValue, String newValue) {
                     try {
                         dp.set(Double.parseDouble(newValue));
-                    } catch (NumberFormatException e) {}
+                    } catch (NumberFormatException e) {
+                    }
                 }
             });
         }
 
-        @Override public Node getEditor() {
+        @Override
+        public Node getEditor() {
             return editor;
         }
     }
 
     static class IntegerPropertyImpl extends Property {
-        private TextBox editor = new TextBox();
+        private TextField editor = new TextField();
 
         IntegerPropertyImpl(String name, final IntegerProperty ip) {
             super(name);
             editor.setText(Double.toString(ip.getValue()));
             ip.addListener(new ChangeListener<Number>() {
-                @Override public void changed(ObservableValue<? extends Number> valueModel, Number oldValue, Number newValue) {
+                @Override
+                public void changed(ObservableValue<? extends Number> valueModel, Number oldValue, Number newValue) {
                     editor.setText(newValue.toString());
                 }
             });
-            editor.rawTextProperty().addListener(new ChangeListener<String>() {
-                @Override public void changed(ObservableValue<? extends String> valueModel, String oldValue, String newValue) {
+            editor.textProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> valueModel, String oldValue, String newValue) {
                     try {
                         ip.set(Integer.parseInt(newValue));
-                    } catch (NumberFormatException e) {}
+                    } catch (NumberFormatException e) {
+                    }
                 }
             });
         }
 
-        @Override public Node getEditor() {
+        @Override
+        public Node getEditor() {
             return editor;
         }
     }
@@ -258,7 +317,8 @@ public class PropertySheet extends GridPane {
             super(name);
             editor.setSelected(bp.getValue());
             bp.addListener(new ChangeListener<Boolean>() {
-                @Override public void changed(ObservableValue<? extends Boolean> valueModel, Boolean oldValue, Boolean newValue) {
+                @Override
+                public void changed(ObservableValue<? extends Boolean> valueModel, Boolean oldValue, Boolean newValue) {
                     editor.setSelected(newValue);
                 }
             });
@@ -270,7 +330,8 @@ public class PropertySheet extends GridPane {
             });
         }
 
-        @Override public Node getEditor() {
+        @Override
+        public Node getEditor() {
             return editor;
         }
     }
@@ -280,24 +341,27 @@ public class PropertySheet extends GridPane {
 
         EnumPropertyImpl(String name, final ObjectProperty<Enum> ep) {
             super(name);
-            Enum val =  ep.get();
+            Enum val = ep.get();
             for (Object enumOpt : val.getDeclaringClass().getEnumConstants()) {
-                editor.getItems().add((Enum)enumOpt);
+                editor.getItems().add((Enum) enumOpt);
             }
             editor.getSelectionModel().select(val);
             ep.addListener(new ChangeListener<Enum>() {
-                @Override public void changed(ObservableValue<? extends Enum> valueModel, Enum oldValue, Enum newValue) {
+                @Override
+                public void changed(ObservableValue<? extends Enum> valueModel, Enum oldValue, Enum newValue) {
                     editor.getSelectionModel().select(newValue);
                 }
             });
             editor.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Enum>() {
-                @Override public void changed(ObservableValue<? extends Enum> valueModel, Enum oldValue, Enum newValue) {
+                @Override
+                public void changed(ObservableValue<? extends Enum> valueModel, Enum oldValue, Enum newValue) {
                     ep.setValue(newValue);
                 }
             });
         }
 
-        @Override public Node getEditor() {
+        @Override
+        public Node getEditor() {
             return editor;
         }
     }
